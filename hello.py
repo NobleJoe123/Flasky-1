d9c6e0da1cd87b52cfc123dd53b94b11d01d2b82
NobleJoe123 <145128278+NobleJoe123@users.noreply.github.com>
2024-02-06 06:48:13 +0100

    add

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from flask import redirect
 from flask import abort
 from flask_bootstrap import Bootstrap
 from datetime import datetime
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
@@ -69,18 +69,18 @@ bootstrap = Bootstrap(app)
 #  return render_template('index.html',
 #  current_time=datetime.utcnow())
  
-class NameForm(Form):
+class NameForm(FlaskForm):
      name = StringField('What is your name?', validators=[DataRequired()])
      submit = SubmitField('Submit')
 
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     name = None
-#     form = NameForm()
-#     if form.validate_on_submit():
-#          name = form.name.data
-#          form.name.data = ''
-#     return render_template('index.html', form=form, name=name)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+         name = form.name.data
+         form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 
 # @app.route('/', methods=['GET', 'POST'])
@@ -91,18 +91,18 @@ class NameForm(Form):
 #      return redirect(url_for('index'))
 #  return render_template('index.html', form=form, name=session.get('name'))
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
-            session['name'] = form.name.data
-            form.name.data = ''
-            return redirect(url_for('index'))
-        return render_template('index.html',
-                               form = form, name = session.get('name'))
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     form = NameForm()
+#     if form.validate_on_submit():
+#         old_name = session.get('name')
+#         if old_name is not None and old_name != form.name.data:
+#             flash('Looks like you have changed your name!')
+#             session['name'] = form.name.data
+#             form.name.data = ''
+#             return redirect(url_for('index'))
+#         return render_template('index.html',
+#                                form = form, name = session.get('name'))
 
 
 if __name__ == '__main__':
